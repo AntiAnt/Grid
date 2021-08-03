73bdb1f015571d16b32d24e32fc1f975dc8867b4
Michael Welsh <welsh.11311@gmail.com>
2021-08-03 16:47:07 -0400

    new approach

--- exercise.py
+++ exercise.py
@@ -1,139 +1,152 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-from fastai.vision.all import *
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
 
 
 
-# In[2]:
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
 
-
-dev = "cuda" if torch.cuda.is_available() else "cpu"
-
-
-# In[3]:
-
-
-path = untar_data(URLs.MNIST)
-zeroes = (path/'training'/'0').ls().sorted()
-ones = (path/'training'/'1').ls().sorted()
-twos = (path/'training'/'2').ls().sorted()
-threes = (path/'training'/'3').ls().sorted()
-fours = (path/'training'/'4').ls().sorted()
-fives = (path/'training'/'5').ls().sorted()
-sixes = (path/'training'/'6').ls().sorted()
-sevens = (path/'training'/'7').ls().sorted()
-eights = (path/'training'/'8').ls().sorted()
-nines = (path/'training'/'9').ls().sorted()
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
 
 
 # In[4]:
 
 
-zeroes_tensor = [tensor(Image.open(o)) for o in zeroes]
-ones_tensor = [tensor(Image.open(o)) for o in ones]
-twos_tensor = [tensor(Image.open(o)) for o in twos]
-threes_tensor = [tensor(Image.open(o)) for o in threes]
-fours_tensor = [tensor(Image.open(o)) for o in fours]
-fives_tensor = [tensor(Image.open(o)) for o in fives]
-sixes_tensor = [tensor(Image.open(o)) for o in sixes]
-sevens_tensor = [tensor(Image.open(o)) for o in sevens]
-eights_tensor = [tensor(Image.open(o)) for o in eights]
-nines_tensor = [tensor(Image.open(o)) for o in nines]
+batch_size = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in test_dataloader:
+    print("Shape of X [N, C, H, W]: ", X.shape)
+    print("Shape of y: ", y.shape, y.dtype)
+    break
 
 
 # In[5]:
 
 
-tensors = [zeroes_tensor, ones_tensor, twos_tensor, threes_tensor, fours_tensor, fives_tensor, sixes_tensor, sevens_tensor, eights_tensor, nines_tensor]
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
 
-
-# In[6]:
-
-
-def split(dset):
-    return train_test_split(dset, test_size=0.2)
-
-def stack_tensors(arr):
-    return [torch.stack(i).float() / 255 for i in arr]
-
-def cat(arr):
-    train_x = torch.cat(arr).view(-1, 28*28)
-    train_y_list = [tensor([i]*len(arr[i])) for i in range(len(arr))]
-    
-    stacked_y = [x for i in train_y_list for x in i]
-
-    train_x = train_x.reshape(-1, 28, 28)
-    train_x = torch.unsqueeze(train_x, 1)
-    train_x = torch.tile(train_x, (1, 3, 1, 1))
-
-    return list(zip(train_x, stacked_y))
 
 
 # In[7]:
 
 
-batch_size = 64
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+
+        # Convolution 1
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=config.channels_one, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        # Max pool 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        # Convolution 2
+        self.cnn2 = nn.Conv2d(in_channels=config.channels_one, out_channels=config.channels_two, kernel_size=5, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+
+        # Max pool 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.dropout = nn.Dropout(p=config.dropout)
+
+        # Fully connected 1 (readout)
+        self.fc1 = nn.Linear(config.channels_two*4*4, 10)
+
+    def forward(self, x):
+        # Convolution 1
+        out = self.cnn1(x)
+        out = self.relu1(out)
+
+        # Max pool 1
+        out = self.maxpool1(out)
+
+        # Convolution 2
+        out = self.cnn2(out)
+        out = self.relu2(out)
+
+        # Max pool 2
+        out = self.maxpool2(out)
+
+        # Resize
+        # Original size: (100, 32, 7, 7)
+        # out.size(0): 100
+        # New out size: (100, 32*7*7)
+        out = out.view(out.size(0), -1)
+        out = self.dropout(out)
+        # Linear function (readout)
+        out = self.fc1(out)
+
+        return out
+
+model = CNNModel().to(device)
+print(model)
 
 
-# In[8]:
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
 
 
-stack = stack_tensors(tensors)
+# In[ ]:
 
 
-# In[9]:
 
 
-cat_stack = cat(stack)
-cat_stack[0]
-
-
-# In[12]:
-
-
-train_ds, valid_ds = split(cat_stack)
-
-
-# In[13]:
-
-
-train_dl = DataLoader(train_ds, batch_size=batch_size)
-valid_dl = DataLoader(valid_ds, batch_size=batch_size)
-
-
-# In[14]:
-
-
-dls = DataLoaders(train_dl, valid_dl)
-
-
-# In[15]:
-
-
-resnet = resnet18().to(dev)
-
-
-# In[16]:
-
-
-trainer = Learner(dls, resnet, loss_func=F.cross_entropy, metrics=error_rate)
-
-
-# In[17]:
-
-
-trainer.fit(n_epoch=4, lr=1e-001)
-
-
-# In[18]:
-
-
-print('DONE!!')
