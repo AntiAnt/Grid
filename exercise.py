2572f1762a46c2ae9900fe22d99b90d134c67883
Michael Welsh <welsh.11311@gmail.com>
2021-08-04 16:21:22 -0400

    added summary writer to get metrics

--- exercise.py
+++ exercise.py
@@ -5,10 +5,12 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
+from torch.utils.tensorboard import SummaryWriter
 
 
 lr = 0.01
 batch_size = 64
+writer = SummaryWriter()
 
 
 if __name__ == '__main__':
@@ -80,7 +82,7 @@ print(f'learning rate: {lr}\nbatch size: {batch_size}')
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
-def train(dataloader, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimizer, epoch):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
@@ -96,6 +98,7 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
+            writer.add_scalar('Loss/train', loss, epoch)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
             
 def test(dataloader, model, loss_fn):
@@ -111,6 +114,7 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
+    writer.add_scalar('Loss/train', )
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 epochs = 10
