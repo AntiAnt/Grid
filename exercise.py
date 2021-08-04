feb6abebc21ea2ae65360ce900c1bc1935c39765
Michael Welsh <welsh.11311@gmail.com>
2021-08-04 16:37:02 -0400

    added tensorboard

--- exercise.py
+++ exercise.py
@@ -7,7 +7,6 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 from torch.utils.tensorboard import SummaryWriter
 
-
 lr = 0.01
 batch_size = 64
 writer = SummaryWriter()
@@ -101,7 +100,7 @@ def train(dataloader, model, loss_fn, optimizer, epoch):
             writer.add_scalar('Loss/train', loss, epoch)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
             
-def test(dataloader, model, loss_fn):
+def test(dataloader, model, loss_fn, epoch):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
@@ -114,12 +113,13 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    writer.add_scalar('Loss/train', )
+    writer.add_scalar('Loss/test', test_loss, epoch)
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 epochs = 10
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    train(train_dataloader, model, loss_fn, optimizer, t)
+    test(test_dataloader, model, loss_fn, t)
 print("Done!")
+writer.close()
